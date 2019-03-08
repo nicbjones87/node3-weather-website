46ecbf77cddf670d678e9adc0338c6bdf03a10b6
Nic Jones <nic@panoramic.health>
2019-03-08 15:57:02 -0800

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode.js');
 const forecast = require('./utils/forecast.js');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // DEFINE PATHS FOR EXPRESS CONFIG
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -92,6 +93,6 @@ app.get('*', (req, res) => {
 	});
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
 	console.log('Server is up on port 3000');
 });
