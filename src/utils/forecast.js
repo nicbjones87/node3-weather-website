aba583bb92f58b77aea54c240cc903d0da62d8f2
Nic Jones <nic@panoramic.health>
2019-03-08 16:13:28 -0800

    Add more forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -16,7 +16,11 @@ const forecast = (lat, long, callback) => {
 					body.currently.temperature +
 					' degrees out.  There is ' +
 					body.currently.precipProbability +
-					'% chance of rain.'
+					"% chance of rain.  Today's high will be " +
+					body.daily.data[0].temperatureHigh +
+					' degrees, and the low will be ' +
+					body.daily.data[0].temperatureLow +
+					' degrees.'
 			);
 		}
 	});
